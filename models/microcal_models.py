8c6a501b601c1b141219f50971c7bfb6e36f2288
eirikama <eamagnussen@gmail.com>
2025-08-05 16:58:28 +0200

    add minimal viable example of training and restructure

--- models/microcal_models.py
+++ models/microcal_models.py
@@ -1,128 +1,62 @@
 import torch
 from .attention_augmented_convolution import AugmentedConv
 
-
-
-class micro_to_macro_model(torch.nn.Module):
+class MicroToMacroModel(torch.nn.Module):
     def __init__(self):
-        super(micro_to_macro_model, self).__init__()
+        super(MicroToMacroModel, self).__init__()
 
-        self.conv_1 = AugmentedConv(
-            in_channels=1,
-            out_channels=16,
-            kernel_size=11,
-            dk=40,
-            dv=4,
-            Nh=4,
-            stride=1,
-            shape=512,
-        )
-        self.conv_2 = AugmentedConv(
-            in_channels=16,
-            out_channels=16,
-            kernel_size=11,
-            dk=40,
-            dv=4,
-            Nh=4,
-            stride=1,
-            shape=256,
-        )
-        self.conv_3 = AugmentedConv(
-            in_channels=16,
-            out_channels=16,
-            kernel_size=11,
-            dk=40,
-            dv=4,
-            Nh=4,
-            stride=1,
-            shape=128,
-        )
-        self.conv_4 = AugmentedConv(
-            in_channels=16,
-            out_channels=3,
-            kernel_size=11,
-            dk=20,
-            dv=2,
-            Nh=2,
-            stride=1,
-            shape=32,
-        )
+        # Define attention augmented convolutional layers
+        self.conv_1 = self._create_augmented_conv(1, 16, 11, 40, 4, 4, 1, 512)
+        self.conv_2 = self._create_augmented_conv(16, 16, 11, 40, 4, 4, 1, 256)
+        self.conv_3 = self._create_augmented_conv(16, 16, 11, 40, 4, 4, 1, 128)
+        self.conv_4 = self._create_augmented_conv(16, 3, 11, 20, 2, 2, 1, 32)
 
-        self.conv_8 = AugmentedConv(
-            in_channels=16,
-            out_channels=16,
-            kernel_size=11,
-            dk=40,
-            dv=4,
-            Nh=4,
-            stride=1,
-            shape=512,
-        )
-        self.conv_7 = AugmentedConv(
-            in_channels=16,
-            out_channels=16,
-            kernel_size=11,
-            dk=40,
-            dv=4,
-            Nh=4,
-            stride=1,
-            shape=256,
-        )
-        self.conv_6 = AugmentedConv(
-            in_channels=16,
-            out_channels=16,
-            kernel_size=11,
-            dk=40,
-            dv=4,
-            Nh=4,
-            stride=1,
-            shape=128,
-        )
+        self.conv_8 = self._create_augmented_conv(16, 16, 11, 40, 4, 4, 1, 512)
+        self.conv_7 = self._create_augmented_conv(16, 16, 11, 40, 4, 4, 1, 256)
+        self.conv_6 = self._create_augmented_conv(16, 16, 11, 40, 4, 4, 1, 128)
+        self.conv_inbetween = self._create_augmented_conv(3, 16, 11, 20, 2, 2, 1, 32)
+        self.conv_5 = self._create_augmented_conv(3, 3, 11, 20, 2, 2, 1, 32)
 
-        self.conv_inbetween = AugmentedConv(
-            in_channels=3,
-            out_channels=16,
-            kernel_size=11,
-            dk=20,
-            dv=2,
-            Nh=2,
-            stride=1,
-            shape=32,
-        )
-        self.conv_5 = AugmentedConv(
-            in_channels=3,
-            out_channels=3,
-            kernel_size=11,
-            dk=20,
-            dv=2,
-            Nh=2,
-            stride=1,
-            shape=32,
-        )
+        # Final convolution layer
+        self.conv_final = torch.nn.Conv1d(in_channels=16, out_channels=1, kernel_size=5, padding=2)
 
-        self.conv_final = torch.nn.Conv1d(
-            in_channels=16, out_channels=1, kernel_size=5, padding=2
-        )
-
+        # Pooling and activation layers
         self.maxpool = torch.nn.MaxPool1d(2, return_indices=True)
         self.maxunpool = torch.nn.MaxUnpool1d(2)
         self.leaky = torch.nn.LeakyReLU()
-        
+
+        # Fully connected layer
         self.fcup = torch.nn.Linear(138, 138)
 
+    def _create_augmented_conv(self, in_channels, out_channels, kernel_size, dk, dv, Nh, stride, shape):
+        return AugmentedConv(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            dk=dk,
+            dv=dv,
+            Nh=Nh,
+            stride=stride,
+            shape=shape,
+        )
+
     def forward(self, x):
+        # Encoder 
         acnn1, idx_1 = self.maxpool(self.leaky(self.conv_1(x)))
         acnn2, idx_2 = self.maxpool(self.leaky(self.conv_2(acnn1)))
         acnn3, idx_3 = self.maxpool(self.leaky(self.conv_3(acnn2)))
         acnn4, idx_4 = self.maxpool(self.leaky(self.conv_4(acnn3)))
+
+        # Fully connected layer
         acnn4_ = acnn4.reshape(acnn4.size(0), -1)
-        
         up = self.leaky(self.fcup(acnn4_))
         up = up.view(acnn4.size())
+
+        # Decoder 
         upacnn1 = self.conv_5(up)
         upsampled1 = self.maxunpool(upacnn1, idx_4, output_size=idx_3.size())
         upsampled1_ = self.conv_inbetween(upsampled1)
-        
+
         upacnn2 = self.conv_6(upsampled1_)
         upsampled2 = self.maxunpool(upacnn2, idx_3, output_size=idx_2.size())
         upacnn3 = self.conv_7(upsampled2)
@@ -130,64 +64,60 @@ class micro_to_macro_model(torch.nn.Module):
         upacnn4 = self.conv_8(upsampled3)
         upsampled4 = self.maxunpool(upacnn4, idx_1, output_size=x.size())
 
+        # Final convolution
         corr = self.conv_final(upsampled4)
-
         return corr
 
 
-
-class calibration_model(torch.nn.Module):
+class CalibrationModel(torch.nn.Module):
     def __init__(self, out_layers):
-        super(calibration_model, self).__init__()
+        super(CalibrationModel, self).__init__()
 
-        self.conv_1 = AugmentedConv(
-            in_channels=1,
-            out_channels=64,
-            kernel_size=17,
-            dk=40,
-            dv=4,
-            Nh=4,
-            stride=1,
-            #             padding=5,
-            shape=128,
-        )
-        self.conv_2 = AugmentedConv(
-            in_channels=64,
-            out_channels=32,
-            kernel_size=11,
-            dk=40,
-            dv=4,
-            Nh=4,
-            stride=1,
-            shape=64,
-        )
-        self.conv_3 = AugmentedConv(
-            in_channels=32,
-            out_channels=3,
-            kernel_size=11,
-            dk=20,
-            dv=2,
-            Nh=2,
-            stride=1,
-            #             padding=5,
-            shape=32,
-        )
+        # Define attention augmented convolutional layers with batch normalization
+        self.conv_1 = self._create_augmented_conv(1, 64, 17, 40, 4, 4, 1, 128)
+        self.bn_1 = torch.nn.BatchNorm1d(64)
 
+        self.conv_2 = self._create_augmented_conv(64, 32, 11, 40, 4, 4, 1, 64)
+        self.bn_2 = torch.nn.BatchNorm1d(32)
+
+        self.conv_3 = self._create_augmented_conv(32, 3, 11, 20, 2, 2, 1, 32)
+        self.bn_3 = torch.nn.BatchNorm1d(3)
+
+        # Pooling and activation layers
         self.maxpool = torch.nn.MaxPool1d(2)
         self.leaky = torch.nn.LeakyReLU()
 
+        # Fully connected layers
         self.fc1 = torch.nn.Linear(276, 128)
         self.fc2 = torch.nn.Linear(128, out_layers)
 
-        self.bn_1 = torch.nn.BatchNorm1d(64)
-        self.bn_2 = torch.nn.BatchNorm1d(32)
-        self.bn_3 = torch.nn.BatchNorm1d(3)
+    def _create_augmented_conv(self, in_channels, out_channels, kernel_size, dk, dv, Nh, stride, shape):
+        return AugmentedConv(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            dk=dk,
+            dv=dv,
+            Nh=Nh,
+            stride=stride,
+            shape=shape,
+        )
 
     def forward(self, x):
-        out = self.bn_1(self.maxpool(self.leaky(self.conv_1(x))))
-        out = self.bn_2(self.maxpool(self.leaky(self.conv_2(out))))
-        out = self.bn_3(self.maxpool(self.leaky(self.conv_3(out))))
+        # Convolution with batch normalization and pooling
+        out = self.bn_1(self.leaky(self.conv_1(x)))
+        out = self.maxpool(out)
+
+        out = self.bn_2(self.leaky(self.conv_2(out)))
+        out = self.maxpool(out)
+
+        out = self.bn_3(self.leaky(self.conv_3(out)))
+        out = self.maxpool(out)
+
+        # Flatten 
         out = out.reshape(out.size(0), -1)
+
+        # Fully connected layers
         out = self.leaky(self.fc1(out))
         out = self.fc2(out)
 
---
+++ train_calibration_model.py
@@ -0,0 +1,52 @@
+import torch
+import numpy as np
+
+import simulation
+from models.microcal_models import CalibrationModel
+
+
+#hyperparameter
+emsc_range = np.array([[0.9, 1.1], [.0, .05], [-0.001, 0.0001], [-0.00005, 0.00005]])
+max_noise = 0.001
+
+lr = 1e-4
+epochs = 10
+batch_size = 8
+
+
+# define model
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+cal_model = CalibrationModel(out_layers=1).to(device).float()
+cal_model.train()
+
+objective_function = torch.nn.MSELoss()
+optimizer = torch.optim.RMSprop(cal_model.parameters(), lr=lr)
+
+
+# load training data (just small set as minimal example)
+data = np.load('../lipid_hts_wn.npz')
+macro_spectra = data['hts']            # macroscopic spectra
+reference_analysis = data['lipid']      # reference analysis values
+wn = data['wn']                        # wavenumbers
+
+
+# train model
+for i in range(epochs):
+    
+    emsc_params = np.array([np.random.uniform(low, high, batch_size) 
+                            for low, high in emsc_range])[:, :, None]
+
+    batch_idc = np.random.randint(0, reference_analysis.shape[0], batch_size)
+    
+    spectra_batch = macro_spectra[batch_idc, :]
+    spectra_batch = simulation.add_polynomial(spectra_batch, wn, emsc_params)
+    spectra_batch = simulation.add_whitenoise(spectra_batch, max_noise)
+    
+    input_tensor = torch.from_numpy(spectra_batch).to(device).float().unsqueeze(1)
+    reference_tensor = torch.from_numpy(reference_analysis[batch_idc]).to(device).float()
+
+    optimizer.zero_grad()
+    outputs = cal_model(input_tensor)
+    loss = objective_function(outputs.squeeze(), reference_tensor)
+    loss.backward()
+    optimizer.step()
