99ede79dd8e053f329fe58689a357770cb78ebb6
eirikama <eamagnussen@gmail.com>
2025-08-05 21:39:29 +0200

    remove unnecessary dependencies

--- inference.py
+++ inference.py
@@ -1,8 +1,6 @@
-import pandas as pd
-from scipy.io import loadmat
-import numpy as np
-from scipy.signal import savgol_filter
 import torch
+import pandas as pd
+import numpy as np
 
 import matplotlib.pyplot as plt 
 import matplotlib as ml
@@ -12,7 +10,6 @@ from biospectools.preprocessing import emsc
 from biospectools.utils.interpolate import interp2wns
 
 from models.microcal_models import CalibrationModel, MicroToMacroModel
-from utils import get_Opus_data
 
 
 wn = np.load('data/wn.npz')['wn']   # wn range used for models
